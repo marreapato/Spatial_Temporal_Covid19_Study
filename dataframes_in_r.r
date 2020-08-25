0365ed617cec00e532875d4627d01679e23bfefb
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-08-25 16:31:25 -0300

    Delete dataframes_in_r.r

--- dataframes_in_r.r
+++
@@ -1,8 +0,0 @@
-library(tidyverse)
-
-#reading the data
-
-recovered=read.csv("data_recovered.csv",header = F)[,-1]
-deaths=read.csv("data_deaths.csv",header = F)[,-1]
-confirmed=read.csv("data_confirmed.csv",header = F)[,-1]
-
