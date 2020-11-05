aeee957083bc0520b137675e90c272938f852e0f
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-11-05 16:33:27 -0300

    Create tables_covi.r

---
+++ R_scripts/tables_covi.r
@@ -0,0 +1,81 @@
+#install.packages("tidyverse")
+#install.packages("COVID19")
+library(COVID19)
+library(tidyverse)
+
+owid <- read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
+owid_jan <- owid %>% filter(date=="2020-01-31")
+owid_feb <- owid %>% filter(date=="2020-02-29")
+owid_mar <- owid %>% filter(date=="2020-03-31")
+owid_ap <- owid %>% filter(date=="2020-04-30")
+owid_may <- owid %>% filter(date=="2020-05-31")
+owid_jun <- owid %>% filter(date=="2020-06-30")
+owid_jul <- owid %>% filter(date=="2020-07-31")
+owid_ag <- owid %>% filter(date=="2020-08-31")
+owid_sep <- owid %>% filter(date=="2020-09-30")
+owid_oct <- owid %>% filter(date=="2020-10-31")
+owid_list=list(jan20=owid_jan,feb20=owid_feb,march20=owid_mar,april20=owid_ap,may20=owid_may,june20=owid_jun,july=owid_jul,
+               ag=owid_ag,sep=owid_sep,oct=owid_oct)
+
+#saving datasets
+jan <-covid19(start ="2020-01-31" ,end ="2020-01-31",raw = F ) 
+feb <-covid19(start ="2020-02-29" ,end ="2020-02-29",raw = F ) 
+march <- covid19(start ="2020-03-31" ,end ="2020-03-31",raw = F ) 
+april <-covid19(start ="2020-04-30" ,end ="2020-04-30",raw = F )  
+may <- covid19(start ="2020-05-31" ,end ="2020-05-31" ,raw = F) 
+june <- covid19(start ="2020-06-30" ,end ="2020-06-30",raw = F ) 
+july <- covid19(start ="2020-07-31" ,end ="2020-07-31",raw = F ) 
+ag <- covid19(start ="2020-08-31" ,end ="2020-08-31",raw = F ) 
+sep <- covid19(start ="2020-09-30" ,end ="2020-09-30",raw = F ) 
+oct <- covid19(start ="2020-10-31" ,end ="2020-10-31",raw = F ) 
+
+#list of datasets
+datasets <- list(jan20=jan,feb20=feb,march20=march,april20=april,may20=may,june20=june,july=july,ag20=ag,sep20=sep,oc20=oct)
+
+#renaming a few cells in the datasets
+
+for(i in 1:length(datasets)){
+  
+  datasets[[i]]$school_closing <- gsub(0, 0, datasets[[i]]$school_closing)
+  datasets[[i]]$school_closing <- gsub(1, 0, datasets[[i]]$school_closing)
+  datasets[[i]]$school_closing <- gsub(2, 0, datasets[[i]]$school_closing)
+  datasets[[i]]$school_closing <- gsub(3, 1, datasets[[i]]$school_closing)
+  
+  datasets[[i]]$workplace_closing <- gsub(0,0, datasets[[i]]$workplace_closing)
+  datasets[[i]]$workplace_closing <- gsub(1, 0, datasets[[i]]$workplace_closing)
+  datasets[[i]]$workplace_closing <- gsub(2, 0, datasets[[i]]$workplace_closing)
+  datasets[[i]]$workplace_closing <- gsub(3, 1, datasets[[i]]$workplace_closing)
+  
+  datasets[[i]]$cancel_events<- gsub(0, 0, datasets[[i]]$cancel_events)
+  datasets[[i]]$cancel_events <- gsub(1, 0, datasets[[i]]$cancel_events)
+  datasets[[i]]$cancel_events <- gsub(2, 1, datasets[[i]]$cancel_events)
+  
+  datasets[[i]]$transport_closing<- gsub(0,0, datasets[[i]]$transport_closing)
+  datasets[[i]]$transport_closing<- gsub(1, 0, datasets[[i]]$transport_closing)
+  datasets[[i]]$transport_closing<- gsub(2, 1, datasets[[i]]$transport_closing)
+  
+  datasets[[i]]$stay_home_restrictions<- gsub(0, 0, datasets[[i]]$stay_home_restrictions)
+  datasets[[i]]$stay_home_restrictions<- gsub(1, 0, datasets[[i]]$stay_home_restrictions)
+  datasets[[i]]$stay_home_restrictions<- gsub(2, 0, datasets[[i]]$stay_home_restrictions)
+  datasets[[i]]$stay_home_restrictions<- gsub(3, 1, datasets[[i]]$stay_home_restrictions)
+  
+  datasets[[i]]$internal_movement_restrictions<- gsub(0, 0, datasets[[i]]$internal_movement_restrictions)
+  datasets[[i]]$internal_movement_restrictions<- gsub(1, 0, datasets[[i]]$internal_movement_restrictions)
+  datasets[[i]]$internal_movement_restrictions<- gsub(2, 1, datasets[[i]]$internal_movement_restrictions)
+  
+  datasets[[i]]$international_movement_restrictions<- gsub(0, 0, datasets[[i]]$international_movement_restrictions)
+  datasets[[i]]$international_movement_restrictions<- gsub(1, 0, datasets[[i]]$international_movement_restrictions)
+  datasets[[i]]$international_movement_restrictions<- gsub(2, 0, datasets[[i]]$international_movement_restrictions)
+  datasets[[i]]$international_movement_restrictions<- gsub(3, 0, datasets[[i]]$international_movement_restrictions)
+  datasets[[i]]$international_movement_restrictions<- gsub(4, 1, datasets[[i]]$international_movement_restrictions)
+  
+  names(datasets[[i]])[names(datasets[[i]]) == "iso_alpha_3"] <- "iso_code"
+  
+  
+}
+
+
+for(i in 1:length(datasets)){
+  datasets[[i]]<-merge(owid_list[[i]],datasets[[i]],by="iso_code")
+}
+
