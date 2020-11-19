62bf57ff32f4e46caa9df54b827af2686bada765
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2020-11-19 16:32:45 -0300

    Update moran_monthly.r

--- R_scripts/moran_monthly.r
+++ R_scripts/moran_monthly.r
@@ -27,6 +27,8 @@ library(spdep)
 world <- ne_countries(scale='medium',returnclass = 'sp')
 
 #saving datasets
+jan <-covid19(start ="2020-01-31" ,end ="2020-01-31",raw = F ) 
+
 feb <-covid19(start ="2020-02-29" ,end ="2020-02-29",raw = F ) 
 
 march <- covid19(start ="2020-03-31" ,end ="2020-03-31",raw = F ) 
@@ -39,9 +41,15 @@ june <- covid19(start ="2020-06-30" ,end ="2020-06-30",raw = F )
 
 july <- covid19(start ="2020-07-31" ,end ="2020-07-31",raw = F ) 
 
+aug <-covid19(start ="2020-08-31" ,end ="2020-08-31",raw = F ) 
+
+sep <-covid19(start ="2020-09-30" ,end ="2020-09-30",raw = F ) 
+
+oct <-covid19(start ="2020-10-31" ,end ="2020-10-31",raw = F ) 
+
 #the right way to do it
 #list of datasets
-datasets <- list(feb20=feb,march20=march,april20=april,may20=may,june20=june,july=july)
+datasets <- list(jan20=jan,feb20=feb,march20=march,april20=april,may20=may,june20=june,july=july,aug20=aug,sep20=sep,oct20=oct)
 
 #renaming a few cells in the datasets
 
@@ -104,8 +112,8 @@ for(i in 1:length(datasets)){
 }
 
 #list of countries to rename in each dataset
-countries <- list(countriesf=datasets$feb20,countriesm=datasets$march20,countriesa=datasets$april20,countriesmay=datasets$may20,
-                  countriesjun=datasets$june20,countriesjul=datasets$july)
+countries <- list(countriesjan=datasets$jan20,countriesf=datasets$feb20,countriesm=datasets$march20,countriesa=datasets$april20,countriesmay=datasets$may20,
+                  countriesjun=datasets$june20,countriesjul=datasets$july,countriesaug=datasets$aug20,countriessep=datasets$sep20,countriesoct=datasets$oct20)
 #renaming a few countries
 
 for(i in 1:length(countries)){
@@ -121,7 +129,8 @@ for(i in 1:length(countries)){
 
 #full datasets of sf object
 
-total <- list(totalf=NULL,totalm=NULL,totala=NULL,totalma=NULL,totaljun=NULL,totaljul=NULL)
+total <- list(totalj=NULL,totalf=NULL,totalm=NULL,totala=NULL,totalma=NULL,totaljun=NULL,totaljul=NULL,totalaug=NULL,totalsep=NULL,totaloct=NULL)
+
 
 for(i in 1:length(total)){
   total[[i]]<-merge(world,countries[[i]],by="subunit")
@@ -228,6 +237,27 @@ for(i in 1:length(total)){
 
 ###################
 #nearest neighbours
+#January
+coor <- coordinates(total$totalj)
+cartePPV3.knn <- knearneigh(coor, k=2) #2 neighbours
+cartePPV3.nb <- knn2nb(cartePPV3.knn,row.names = total$totalj$name)
+PPV3.w <- nb2listw(cartePPV3.nb, style = "W", zero.policy = TRUE)#norm by row
+
+plot(total$totalj, col='gray', border='blue', lwd=2,main= "Vizinhos")
+plot(PPV3.w, coordinates(total$totalj), col='red', lwd=2, add=TRUE)#links
+#lots of variables missing
+
+#monthly
+#death_pop
+moran.plot(total$totalj$death_pop_ratio, PPV3.w, zero.policy=TRUE)
+moran.test(total$totalj$death_pop_ratio,PPV3.w,zero.policy = TRUE,na.action = na.omit)
+moran.mc(nsim=10000,total$totalj$death_pop_ratio,PPV3.w,zero.policy = TRUE,na.action = na.omit)
+#validated
+
+
+
+###################
+#nearest neighbours
 #February
 coor <- coordinates(total$totalf)
 cartePPV3.knn <- knearneigh(coor, k=2) #2 neighbours
@@ -284,6 +314,12 @@ moran.test(total$totalf$recov_pop_ratio,PPV3.w,zero.policy = TRUE,na.action = na
 #tests_pop
 moran.plot(total$totalf$test_pop_ratio, PPV3.w, zero.policy=TRUE)
 moran.test(total$totalf$test_pop_ratio,PPV3.w,zero.policy = TRUE,na.action = na.omit)
+#monthly
+#death_pop
+moran.plot(total$totalf$m_death_pop_ratio, PPV3.w, zero.policy=TRUE)
+moran.test(total$totalf$m_death_pop_ratio,PPV3.w,zero.policy = TRUE,na.action = na.omit)
+moran.mc(nsim=10000,total$totalf$m_death_pop_ratio,PPV3.w,zero.policy = TRUE,na.action = na.omit)
+#validated
 
 #March
 coor <- coordinates(total$totalm)
@@ -1102,62 +1138,59 @@ grid.arrange(ck1,ck2,ck3,ck4,top="Índice de Moran local sobre o número cumulat
 
 grid.arrange(kc1,kc2,kc3,kc4,top="Índice de Moran local sobre o número mensal \n de mortes por habitantes.")
 
-########################3
-                                                                          2.2), bty="n", cex=0.8, y.intersp=0.8)
-################################################################
+###################
+#nearest neighbours
+#august
+coor <- coordinates(total$totalaug)
+cartePPV3.knn <- knearneigh(coor, k=2) #2 neighbours
+cartePPV3.nb <- knn2nb(cartePPV3.knn,row.names = total$totalaug$name)
+PPV3.w <- nb2listw(cartePPV3.nb, style = "W", zero.policy = TRUE)#norm by row
 
-#testing local moran manner
+plot(total$totalaug, col='gray', border='blue', lwd=2,main= "Vizinhos")
+plot(PPV3.w, coordinates(total$totalaug), col='red', lwd=2, add=TRUE)#links
+#lots of variables missing
 
+#monthly
+#death_pop
+moran.plot(total$totalaug$m_death_pop_ratio, PPV3.w, zero.policy=TRUE)
+moran.test(total$totalaug$m_death_pop_ratio,PPV3.w,zero.policy = TRUE,na.action = na.omit)
+moran.mc(nsim=10000,total$totalaug$m_death_pop_ratio,PPV3.w,zero.policy = TRUE,na.action = na.omit)
+#validated
 
-#monthly death pop local
-local.mi.prod<-localmoran(total$totaljul$m_death_pop_ratio, PPV3.w)
+###################
+#nearest neighbours
+#september
+coor <- coordinates(total$totalsep)
+cartePPV3.knn <- knearneigh(coor, k=2) #2 neighbours
+cartePPV3.nb <- knn2nb(cartePPV3.knn,row.names = total$totalsep$name)
+PPV3.w <- nb2listw(cartePPV3.nb, style = "W", zero.policy = TRUE)#norm by row
 
-total$totaljul$lmi<-local.mi.prod[,1]
+plot(total$totalsep, col='gray', border='blue', lwd=2,main= "Vizinhos")
+plot(PPV3.w, coordinates(total$totalsep), col='red', lwd=2, add=TRUE)#links
+#lots of variables missing
 
-total$totaljul$lmi.p<-local.mi.prod[,5]
+#monthly
+#death_pop
+moran.plot(total$totalsep$m_death_pop_ratio, PPV3.w, zero.policy=TRUE)
+moran.test(total$totalsep$m_death_pop_ratio,PPV3.w,zero.policy = TRUE,na.action = na.omit)
+moran.mc(nsim=10000,total$totalsep$m_death_pop_ratio,PPV3.w,zero.policy = TRUE,na.action = na.omit)
+#validated
 
-total$totaljul$lmi.p.sig<-as.factor(ifelse(local.mi.prod[,5]<.001,"Sig p<.001",
-                                           ifelse(local.mi.prod[,5]<.05,"Sig p<.05", "NS" )))
+#nearest neighbours
+#october
+coor <- coordinates(total$totaloct)
+cartePPV3.knn <- knearneigh(coor, k=2) #2 neighbours
+cartePPV3.nb <- knn2nb(cartePPV3.knn,row.names = total$totaloct$name)
+PPV3.w <- nb2listw(cartePPV3.nb, style = "W", zero.policy = TRUE)#norm by row
 
-#require("RColorBrewer")
+plot(total$totaloct, col='gray', border='blue', lwd=2,main= "Vizinhos")
+plot(PPV3.w, coordinates(total$totaloct), col='red', lwd=2, add=TRUE)#links
+#lots of variables missing
 
-#require("sp")
+#monthly
+#death_pop
+moran.plot(total$totaloct$m_death_pop_ratio, PPV3.w, zero.policy=TRUE)
+moran.test(total$totaloct$m_death_pop_ratio,PPV3.w,zero.policy = TRUE,na.action = na.omit)
+moran.mc(nsim=10000,total$totaloct$m_death_pop_ratio,PPV3.w,zero.policy = TRUE,na.action = na.omit)
+#validated
 
-spplot(total$totaljul, "lmi", at=summary(total$totaljul$lmi), col.regions=brewer.pal(5,"RdBu"), main="Local Moran's")
-kc4=spplot(total$totaljul, "lmi.p.sig", col.regions=c("white", "#E6550D","#FDAE6B"), main = "Julho")
-?spplot
-
-
-####
-
-
-
-quadrant <- vector(mode="numeric",length=nrow(local.mi.prod))
-
-# centers the variable of interest around its mean
-m.qualification <- total$totaljul$m_death_pop_ratio - mean(total$totaljul$m_death_pop_ratio)     
-
-# centers the local Moran's around the mean
-m.local <- local.mi.prod[,1] - mean(local.mi.prod[,1])    
-
-# significance threshold
-signif <- 0.05 
-
-# builds a data quadrant
-#positions
-quadrant[m.qualification >0 & m.local>0] <- 4  
-quadrant[m.qualification <0 & m.local<0] <- 1      
-quadrant[m.qualification <0 & m.local>0] <- 2
-quadrant[m.qualification >0 & m.local<0] <- 3
-quadrant[local.mi.prod[,5]>signif] <- 0#you can choose not to run it
-
-
-# plot in r
-brks <- c(0,1,2,3,4)
-colors <- c("white","blue",rgb(0,0,1,alpha=0.4),rgb(1,0,0,alpha=0.4),"red")
-plot(total$totaljul,border="lightgray",col=colors[findInterval(quadrant,brks,all.inside=FALSE)])
-box()
-legend("bottomleft", legend = c("insignificant","low-low","low-high","high-low","high-high"),
-       fill=colors,bty="n")
-
-#https://rpubs.com/quarcs-lab/spatial-autocorrelation
