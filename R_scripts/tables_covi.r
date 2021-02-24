2820cddf88f430870198f529df0bae5cf6d822b6
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2021-02-24 15:41:45 -0300

    Update tables_covi.r

--- R_scripts/tables_covi.r
+++ R_scripts/tables_covi.r
@@ -2,7 +2,6 @@
 #install.packages("COVID19")
 library(COVID19)
 library(tidyverse)
-
 owid <- read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
 owid_jan <- owid %>% filter(date=="2020-01-31")
 owid_feb <- owid %>% filter(date=="2020-02-29")
@@ -14,8 +13,13 @@ owid_jul <- owid %>% filter(date=="2020-07-31")
 owid_ag <- owid %>% filter(date=="2020-08-31")
 owid_sep <- owid %>% filter(date=="2020-09-30")
 owid_oct <- owid %>% filter(date=="2020-10-31")
+owid_nov <- owid %>% filter(date=="2020-11-30")
+owid_dec <- owid %>% filter(date=="2020-12-31")
+
 owid_list=list(jan20=owid_jan,feb20=owid_feb,march20=owid_mar,april20=owid_ap,may20=owid_may,june20=owid_jun,july=owid_jul,
-               ag=owid_ag,sep=owid_sep,oct=owid_oct)
+               ag=owid_ag,sep=owid_sep,oct=owid_oct,no=owid_nov,de=owid_dec)
+
+
 
 #saving datasets
 jan <-covid19(start ="2020-01-31" ,end ="2020-01-31",raw = F ) 
@@ -28,9 +32,11 @@ july <- covid19(start ="2020-07-31" ,end ="2020-07-31",raw = F )
 ag <- covid19(start ="2020-08-31" ,end ="2020-08-31",raw = F ) 
 sep <- covid19(start ="2020-09-30" ,end ="2020-09-30",raw = F ) 
 oct <- covid19(start ="2020-10-31" ,end ="2020-10-31",raw = F ) 
+nov <-covid19(start ="2020-11-30" ,end ="2020-11-30",raw = F ) 
+dec <-covid19(start ="2020-12-31" ,end ="2020-12-31",raw = F ) 
 
 #list of datasets
-datasets <- list(jan20=jan,feb20=feb,march20=march,april20=april,may20=may,june20=june,july=july,ag20=ag,sep20=sep,oc20=oct)
+datasets <- list(jan20=jan,feb20=feb,march20=march,april20=april,may20=may,june20=june,july=july,ag20=ag,sep20=sep,oc20=oct,nov20=nov,dec20=dec)
 
 #renaming a few cells in the datasets
 
@@ -82,71 +88,71 @@ for(i in 1:length(datasets)){
 #School  closures mar world, europe, sa, na
 continents=list(w=c(sc=list(),work=list()),eu=c(sc=list(),work=list()),sa=c(sc=list(),work=list()),na=c(sc=list(),work=list()))
 for(i in 1:length(datasets)){
- 
-continents$w$sc[[i]]=data.frame(table(datasets[[i]]$school_closing))
-
-continents$eu$sc[[i]]=as.data.frame(table(datasets[[i]]$school_closing[datasets[[i]]$continent=="Europe"]))
-
-continents$sa$sc[[i]]=data.frame(table(datasets[[i]]$school_closing[datasets[[i]]$continent=="South America"]))
-
-continents$na$sc[[i]]=data.frame(table(datasets[[i]]$school_closing[datasets[[i]]$continent=="North America"]))
-
-continents$w$work[[i]]=data.frame(table(datasets[[i]]$workplace_closing))
-
-continents$eu$work[[i]]=data.frame(table(datasets[[i]]$workplace_closing[datasets[[i]]$continent=="Europe"]))
-
-continents$sa$work[[i]]=data.frame(table(datasets[[i]]$workplace_closing[datasets[[i]]$continent=="South America"]))
-
-continents$na$work[[i]]=data.frame(table(datasets[[i]]$workplace_closing[datasets[[i]]$continent=="North America"]))
-
-continents$w$events[[i]]=data.frame(table(datasets[[i]]$cancel_events))
-
-continents$eu$events[[i]]=data.frame(table(datasets[[i]]$cancel_events[datasets[[i]]$continent=="Europe"]))
-
-continents$sa$events[[i]]=data.frame(table(datasets[[i]]$cancel_events[datasets[[i]]$continent=="South America"]))
-
-continents$na$events[[i]]=data.frame(table(datasets[[i]]$cancel_events[datasets[[i]]$continent=="North America"]))
-
-#########
-
-continents$w$transp[[i]]=data.frame(table(datasets[[i]]$transport_closing))
-
-continents$eu$transp[[i]]=data.frame(table(datasets[[i]]$transport_closing[datasets[[i]]$continent=="Europe"]))
-
-continents$sa$transp[[i]]=data.frame(table(datasets[[i]]$transport_closing[datasets[[i]]$continent=="South America"]))
-
-continents$na$transp[[i]]=data.frame(table(datasets[[i]]$transport_closing[datasets[[i]]$continent=="North America"]))
-
-#########
-
-continents$w$home[[i]]=data.frame(table(datasets[[i]]$stay_home_restrictions))
-
-continents$eu$home[[i]]=data.frame(table(datasets[[i]]$stay_home_restrictions[datasets[[i]]$continent=="Europe"]))
-
-continents$sa$home[[i]]=data.frame(table(datasets[[i]]$stay_home_restrictions[datasets[[i]]$continent=="South America"]))
-
-continents$na$home[[i]]=data.frame(table(datasets[[i]]$stay_home_restrictions[datasets[[i]]$continent=="North America"]))
-
-#########
-
-continents$w$internal[[i]]=data.frame(table(datasets[[i]]$internal_movement_restrictions))
-
-continents$eu$internal[[i]]=data.frame(table(datasets[[i]]$internal_movement_restrictions[datasets[[i]]$continent=="Europe"]))
-
-continents$sa$internal[[i]]=data.frame(table(datasets[[i]]$internal_movement_restrictions[datasets[[i]]$continent=="South America"]))
-
-continents$na$internal[[i]]=data.frame(table(datasets[[i]]$internal_movement_restrictions[datasets[[i]]$continent=="North America"]))
-
-#########
-
-continents$w$international[[i]]=data.frame(table(datasets[[i]]$international_movement_restrictions))
-
-continents$eu$international[[i]]=data.frame(table(datasets[[i]]$international_movement_restrictions[datasets[[i]]$continent=="Europe"]))
-
-continents$sa$international[[i]]=data.frame(table(datasets[[i]]$international_movement_restrictions[datasets[[i]]$continent=="South America"]))
-
-continents$na$international[[i]]=data.frame(table(datasets[[i]]$international_movement_restrictions[datasets[[i]]$continent=="North America"]))
-
+  
+  continents$w$sc[[i]]=data.frame(table(datasets[[i]]$school_closing))
+  
+  continents$eu$sc[[i]]=as.data.frame(table(datasets[[i]]$school_closing[datasets[[i]]$continent=="Europe"]))
+  
+  continents$sa$sc[[i]]=data.frame(table(datasets[[i]]$school_closing[datasets[[i]]$continent=="South America"]))
+  
+  continents$na$sc[[i]]=data.frame(table(datasets[[i]]$school_closing[datasets[[i]]$continent=="North America"]))
+  
+  continents$w$work[[i]]=data.frame(table(datasets[[i]]$workplace_closing))
+  
+  continents$eu$work[[i]]=data.frame(table(datasets[[i]]$workplace_closing[datasets[[i]]$continent=="Europe"]))
+  
+  continents$sa$work[[i]]=data.frame(table(datasets[[i]]$workplace_closing[datasets[[i]]$continent=="South America"]))
+  
+  continents$na$work[[i]]=data.frame(table(datasets[[i]]$workplace_closing[datasets[[i]]$continent=="North America"]))
+  
+  continents$w$events[[i]]=data.frame(table(datasets[[i]]$cancel_events))
+  
+  continents$eu$events[[i]]=data.frame(table(datasets[[i]]$cancel_events[datasets[[i]]$continent=="Europe"]))
+  
+  continents$sa$events[[i]]=data.frame(table(datasets[[i]]$cancel_events[datasets[[i]]$continent=="South America"]))
+  
+  continents$na$events[[i]]=data.frame(table(datasets[[i]]$cancel_events[datasets[[i]]$continent=="North America"]))
+  
+  #########
+  
+  continents$w$transp[[i]]=data.frame(table(datasets[[i]]$transport_closing))
+  
+  continents$eu$transp[[i]]=data.frame(table(datasets[[i]]$transport_closing[datasets[[i]]$continent=="Europe"]))
+  
+  continents$sa$transp[[i]]=data.frame(table(datasets[[i]]$transport_closing[datasets[[i]]$continent=="South America"]))
+  
+  continents$na$transp[[i]]=data.frame(table(datasets[[i]]$transport_closing[datasets[[i]]$continent=="North America"]))
+  
+  #########
+  
+  continents$w$home[[i]]=data.frame(table(datasets[[i]]$stay_home_restrictions))
+  
+  continents$eu$home[[i]]=data.frame(table(datasets[[i]]$stay_home_restrictions[datasets[[i]]$continent=="Europe"]))
+  
+  continents$sa$home[[i]]=data.frame(table(datasets[[i]]$stay_home_restrictions[datasets[[i]]$continent=="South America"]))
+  
+  continents$na$home[[i]]=data.frame(table(datasets[[i]]$stay_home_restrictions[datasets[[i]]$continent=="North America"]))
+  
+  #########
+  
+  continents$w$internal[[i]]=data.frame(table(datasets[[i]]$internal_movement_restrictions))
+  
+  continents$eu$internal[[i]]=data.frame(table(datasets[[i]]$internal_movement_restrictions[datasets[[i]]$continent=="Europe"]))
+  
+  continents$sa$internal[[i]]=data.frame(table(datasets[[i]]$internal_movement_restrictions[datasets[[i]]$continent=="South America"]))
+  
+  continents$na$internal[[i]]=data.frame(table(datasets[[i]]$internal_movement_restrictions[datasets[[i]]$continent=="North America"]))
+  
+  #########
+  
+  continents$w$international[[i]]=data.frame(table(datasets[[i]]$international_movement_restrictions))
+  
+  continents$eu$international[[i]]=data.frame(table(datasets[[i]]$international_movement_restrictions[datasets[[i]]$continent=="Europe"]))
+  
+  continents$sa$international[[i]]=data.frame(table(datasets[[i]]$international_movement_restrictions[datasets[[i]]$continent=="South America"]))
+  
+  continents$na$international[[i]]=data.frame(table(datasets[[i]]$international_movement_restrictions[datasets[[i]]$continent=="North America"]))
+  
 }
 
 continents$na$international[[10]]
