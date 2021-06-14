db41140759d82140e2ff82cb6f4dd1cd3d77ea39
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2021-06-14 08:56:52 -0300

    Update ui.R

--- spatial_BR-APP/ui.R
+++ spatial_BR-APP/ui.R
@@ -21,7 +21,8 @@ shinyUI(fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotlyOutput("distPlot")
+            plotlyOutput("distPlot"),
+            plotlyOutput("distPlot2")
         )
     )
 ))
