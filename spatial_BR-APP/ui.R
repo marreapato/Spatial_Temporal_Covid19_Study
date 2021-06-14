d4ec9b154be8d61057c1c69073c1a67024b1a5a2
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2021-06-14 09:09:37 -0300

    Update ui.R

--- spatial_BR-APP/ui.R
+++ spatial_BR-APP/ui.R
@@ -1,28 +1,136 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#install.packages("tidyverse")
+#install.packages("ggthemes")
+#install.packages("readxl")
+#install.packages("maptools")
+#install.packages("sf")
+#install.packages("plotly",dependencies = TRUE)
+#install.packages("tmap")
+#install.packages("geobr")
+#install.packages("spdep")
+#install.packages("sf")
+#install.packages("crul")
+#install.packages("rgdal")
+#install.packages('spDataLarge',
+#           repos='https://nowosad.github.io/drat/', type='source')
+
+#install.packages('rlang')
+
+#install.packages("leaflet")
+#install.packages("htmltools")
+library(leaflet)
+library(crul)
+library(sf)
+library(spdep)
+library(geobr)
+library(tmap)
+library(tidyverse)
+library(readxl)
+library(rgdal)
+library(maptools)
+library(plotly)
+
+data <- reactive({
+    invalidateLater(4.32e+7/4,NULL)
+    df_br = read.csv("https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv")
+}) 
+
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    
+    output$distPlot <-  renderPlotly({
+        
+        ###################################
+        #analisarei a crime_rate
+        df_br <- data()
+        df_br <- df_br%>%group_by(date,state)%>%
+            summarise_if(is.numeric,sum,na.rm=T)
+        
+        df_br <- df_br %>% filter(state!="TOTAL")
+        ########################################
+        
+        df_br <- df_br[,c(1,2,6)]
+        
+        df_br <- df_br %>% filter(date==Sys.Date())
+        
+        #Mesorregioes#geo_br
+        mesos <- read_state(year=2017,simplified = T)
+        
+        mesos <- mesos[,c(2,6)]
+        
+        #estados
+        
+        df_br <- as_tibble(df_br)
+        
+        mesos_sp <- left_join(mesos,df_br, by = c("abbrev_state" = "state"))
+        
+        colnames(mesos_sp)[3] <- "Casos"
+        
+        mp <- ggplot() +
+                geom_sf(data=mesos_sp,aes(fill=Casos), size=.15) +
+                labs(title="Mapa de Novos Casos de COVID-19 no Brasil", size=8,fill="Casos") +
+                theme(legend.position = "right",axis.title.x=element_blank(),
+                                        axis.text.x=element_blank(),
+                                        axis.ticks.x=element_blank(),axis.title.y=element_blank(),
+                                        axis.text.y=element_blank(),
+                                        axis.ticks.y=element_blank())
+        
+        
+        
+        
+        
+        ggplotly(mp)
+        
+    })
+    
+    
+    output$distPlot2 <-  renderPlotly({
+        
+        ###################################
+        #analisarei a crime_rate
+        df_br <- data()
+        df_br <- df_br%>%group_by(date,state)%>%
+            summarise_if(is.numeric,sum,na.rm=T)
+        
+        df_br <- df_br %>% filter(state!="TOTAL")
+        ########################################
+        
+        df_br <- df_br[,c(1,2,6)]
+        
+        df_br <- df_br %>% filter(date==Sys.Date()-1)
+        
+        #Mesorregioes#geo_br
+        mesos <- read_state(year=2017,simplified = T)
+        
+        mesos <- mesos[,c(2,6)]
+        
+        #estados
+        
+        df_br <- as_tibble(df_br)
+        
+        mesos_sp <- left_join(mesos,df_br, by = c("abbrev_state" = "state"))
+        
+        colnames(mesos_sp)[3] <- "Casos"
+        
+        mp <- ggplot() +
+            geom_sf(data=mesos_sp,aes(fill=Casos), size=.15) +
+            labs(title="Mapa de Casos de COVID-19 no Brasil ontem", size=8,fill="Casos") +
+            theme(legend.position = "right",axis.title.x=element_blank(),
+                                    axis.text.x=element_blank(),
+                                    axis.ticks.x=element_blank(),axis.title.y=element_blank(),
+                                    axis.text.y=element_blank(),
+                                    axis.ticks.y=element_blank())
+        
+        
+        
+        
+        
+        ggplotly(mp)
+        
+    })
+    
+    
+})
 
-    # Application title
-    titlePanel("Quantidade atualizada de casos de Covid-19 no Brasil, \n dados apresentados são por data de divulgação dos boletins de cada unidade federativa."),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel("Atualizações de 3 em 3 horas,\n fonte:W. Cota, “Monitoring the number of COVID-19 cases and deaths in brazil at municipal and federative units level”, SciELOPreprints:362 (2020), 10.1590/scielopreprints.362"),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotlyOutput("distPlot"),
-            plotlyOutput("distPlot2")
-        )
-    )
-))
