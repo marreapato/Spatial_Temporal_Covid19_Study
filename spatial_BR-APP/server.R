55f4baed61cd717e81148f7e151f46d96428f223
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2021-06-14 08:56:25 -0300

    Update server.R

--- spatial_BR-APP/server.R
+++ spatial_BR-APP/server.R
@@ -70,8 +70,8 @@ shinyServer(function(input, output) {
         
         mp <- ggplot() +
                 geom_sf(data=mesos_sp,aes(fill=Casos), size=.15) +
-                labs(subtitle="Mapa de Novos Casos de COVID-19 no Brasil", size=8,fill="Casos") +
-                theme_minimal()  +theme(legend.position = "right",axis.title.x=element_blank(),
+                labs(title="Mapa de Novos Casos de COVID-19 no Brasil", size=8,fill="Casos") +
+                theme(legend.position = "right",axis.title.x=element_blank(),
                                         axis.text.x=element_blank(),
                                         axis.ticks.x=element_blank(),axis.title.y=element_blank(),
                                         axis.text.y=element_blank(),
@@ -84,6 +84,53 @@ shinyServer(function(input, output) {
         ggplotly(mp)
         
     })
+    
+    
+    output$distPlot2 <-  renderPlotly({
+        
+        ###################################
+        #analisarei a crime_rate
+        df_br <- data()
+        df_br <- df_br%>%group_by(date,state)%>%
+            summarise_if(is.numeric,sum,na.rm=T)
+        
+        df_br <- df_br %>% filter(state!="TOTAL")
+        ########################################
+        
+        df_br <- df_br[,c(1,2,6)]
+        
+        df_br <- df_br %>% filter(date==Sys.Date()-1)
+        
+        #Mesorregioes#geo_br
+        mesos <- read_state(year=2017,simplified = T)
+        
+        mesos <- mesos[,c(2,6)]
+        
+        #estados
+        
+        df_br <- as_tibble(df_br)
+        
+        mesos_sp <- left_join(mesos,df_br, by = c("abbrev_state" = "state"))
+        
+        colnames(mesos_sp)[3] <- "Casos"
+        
+        mp <- ggplot() +
+            geom_sf(data=mesos_sp,aes(fill=Casos), size=.15) +
+            labs(title="Mapa de Casos de COVID-19 no Brasil ontem", size=8,fill="Casos") +
+            theme(legend.position = "right",axis.title.x=element_blank(),
+                                    axis.text.x=element_blank(),
+                                    axis.ticks.x=element_blank(),axis.title.y=element_blank(),
+                                    axis.text.y=element_blank(),
+                                    axis.ticks.y=element_blank())
+        
+        
+        
+        
+        
+        ggplotly(mp)
+        
+    })
+    
     
 })
 
