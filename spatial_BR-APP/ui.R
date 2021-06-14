55cd349e2eada3618274aed65cb59990217c3236
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2021-06-14 08:49:13 -0300

    Update ui.R

--- spatial_BR-APP/ui.R
+++ spatial_BR-APP/ui.R
@@ -13,15 +13,15 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Quantidade atualizada de casos de Covid-19 no Brasil"),
+    titlePanel("Quantidade atualizada de casos de Covid-19 no Brasil, \n dados apresentados são por data de divulgação dos boletins de cada unidade federativa."),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
-        sidebarPanel("Atualizações de 6 em 6 horas"),
+        sidebarPanel("Atualizações de 3 em 3 horas,\n fonte:W. Cota, “Monitoring the number of COVID-19 cases and deaths in brazil at municipal and federative units level”, SciELOPreprints:362 (2020), 10.1590/scielopreprints.362"),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            plotlyOutput("distPlot")
         )
     )
 ))
