69ff58f1e6cfc93448b8a93e64c1427ea6343fc6
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2021-06-20 16:54:02 -0300

    Update ui.R

--- spatial_BR-APP/ui.R
+++ spatial_BR-APP/ui.R
@@ -5,15 +5,59 @@
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
-#
+#                    useShinyjs()
 
+#install.packages("thematic")
+#https://unleash-shiny.rinterface.com/web-intro.html
 library(shiny)
 library(plotly)
+library(bslib)
+library(thematic)
+
+theme_toggle <- function() {
+    div(
+        class = "custom-control custom-switch", 
+        tags$input(
+            id = "custom_mode", 
+            type = "checkbox", 
+            class = "custom-control-input",
+            onclick = HTML(
+                "Shiny.setInputValue(
+          'dark_mode', 
+          document.getElementById('custom_mode').value
+        );"
+            )
+        ),
+        tags$label(
+            "Custom mode?", 
+            `for` = "custom_mode", 
+            class = "custom-control-label"
+        )
+    )
+}
+
+neon_theme <- bs_theme(
+    bg = "#000000", 
+    fg = "#FFFFFF", 
+    primary = "#9600FF", 
+    secondary = "#1900A0",
+    success = "#38FF12",
+    info = "#00F5FB",
+    warning = "#FFF100",
+    danger = "#FF00E3",
+    base_font = "Marker Felt",
+    heading_font = "Marker Felt",
+    code_font = "Chalkduster"
+)
+#bs_theme_preview(neon_theme, with_themer = FALSE)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
+    theme = neon_theme, 
+    theme_toggle(),
     img(src='https://intercom2020.ufba.br/wp-content/uploads/2020/11/ufbalogo.png', align = "right"),
     # Application title
+
     titlePanel("Quantidade atualizada de casos de Covid-19 no Brasil, \n dados apresentados são por data de divulgação dos boletins de cada unidade federativa."),
 
     # Sidebar with a slider input for number of bins
